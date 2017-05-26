8f2fe221e7c892c842b598c76309334421d7d304
sacherjj <sacherjj@gmail.com>
2017-05-26 19:38:19 -0400

    Updates to make documentation build.

--- simple_zpl2/printer.py
+++ simple_zpl2/printer.py
@@ -8,6 +8,12 @@ class NetworkPrinter(object):
     """
 
     def __init__(self, ip_address, port=9100):
+        """
+        Constructor
+        
+        :param ip_address: printer network address as string ('xxx.xxx.xxx.xxx')
+        :param port: port of printer as int (default 9100)
+        """
         self.ip = ip_address
         self.port = port
 
@@ -16,6 +22,7 @@ class NetworkPrinter(object):
         Send ZPL2 formatted text to a network label printer
         
         :param label_formatter: Formatter object, fully build for label.
+        :param timeout: Socket timeout for printer connection, default 10.
         :return: None
         """
         with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
