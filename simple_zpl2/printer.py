43aff360f5fe29d293816cf3f5dc015d1485a1d5
sacherjj <sacherjj@gmail.com>
2017-05-30 22:50:49 -0400

    Cleaning up doc strings

--- simple_zpl2/printer.py
+++ simple_zpl2/printer.py
@@ -4,15 +4,12 @@ import socket
 class NetworkPrinter(object):
     """
     Object to send ZPL to zebra network printer using sockets
+
+    :param ip_address: printer network address as 'xxx.xxx.xxx.xxx'
+    :param port: port of printer as int (default 9100)
     """
 
     def __init__(self, ip_address, port=9100):
-        """
-        Constructor
-
-        :param ip_address: printer network address as 'xxx.xxx.xxx.xxx'
-        :param port: port of printer as int (default 9100)
-        """
         self.ip = ip_address
         self.port = port
 
@@ -22,7 +19,6 @@ class NetworkPrinter(object):
 
         :param label_formatter: Formatter object, fully build for label.
         :param timeout: Socket timeout for printer connection, default 10.
-        :return: None
         """
         with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
             s.settimeout(timeout)
