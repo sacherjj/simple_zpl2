0ccff96a560dfd848d2f6edf3bb77abd473116bb
sacherjj <sacherjj@gmail.com>
2017-05-31 11:14:10 -0400

    Updating version.

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ test_requirements = [
 
 setup(
     name='simple_zpl2',
-    version='0.1.0',
+    version='0.2.1',
     description="For building ZPL2 strings for printing barcodes with Zebra or compatible label printers.",
     long_description=readme + '\n\n' + history,
     author="Joe Sacher",
